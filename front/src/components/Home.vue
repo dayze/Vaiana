c088f22a3c0fecf7726443a0b026ed1c5cd7603d
Jeremy Habit <jeremy.habit@gmail.com>
2017-12-08 15:19:17 +0100

    init front

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -17,34 +17,23 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <search-form></search-form>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Maxime se fait raketté ! =('
+  import SearchForm from '@/components/SearchForm'
+
+  export default {
+    components: {SearchForm},
+    name: 'Home',
+    component: {
+      SearchForm
+    },
+    data () {
+      return {
+        msg: 'Maxime se fait raketté ! =('
+      }
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
