8aa4782d44cbae8a3d184a01b90f146c29013f04
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2017-12-13 16:15:33 +0100

    Transform CSS with MM.

--- front/src/components/layouts/SearchFiltersDesktop.vue
+++ front/src/components/layouts/SearchFiltersDesktop.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="show hide-s" id="filter-desktop">
+  <div class="show hide-s border-right border-silver-right" id="filter-desktop">
     <form class="flex-form flex-column styled-form">
       <h4>Classement</h4>
-      <ul>
-        <li><input id="ranking-5-star" type="checkbox"/><label for="ranking-5-star">5 étoiles</label></li>
-        <li><input id="ranking-4-star" type="checkbox"/><label for="ranking-4-star">4 étoiles</label></li>
-        <li><input id="ranking-3-star" type="checkbox"/><label for="ranking-3-star">3 étoiles</label></li>
-        <li><input id="ranking-2-star" type="checkbox"/><label for="ranking-2-star">2 étoiles</label></li>
-        <li><input id="ranking-1-star" type="checkbox"/><label for="ranking-1-star">1 étoile</label></li>
+      <ul class="margin-0 padding-0 list-unstyled">
+        <li><input id="ranking-5-star" class="cursor-pointer" type="checkbox"/><label for="ranking-5-star">5 étoiles</label></li>
+        <li><input id="ranking-4-star" class="cursor-pointer" type="checkbox"/><label for="ranking-4-star">4 étoiles</label></li>
+        <li><input id="ranking-3-star" class="cursor-pointer" type="checkbox"/><label for="ranking-3-star">3 étoiles</label></li>
+        <li><input id="ranking-2-star" class="cursor-pointer" type="checkbox"/><label for="ranking-2-star">2 étoiles</label></li>
+        <li><input id="ranking-1-star" class="cursor-pointer" type="checkbox"/><label for="ranking-1-star">1 étoile</label></li>
       </ul>
       <h4>Avis client</h4>
-      <ul>
+      <ul class="margin-0 padding-0 list-unstyled">
         <li>
           <div class="rating">
             <a><span class="fill-star">★</span><span class="fill-star">★</span><span class="fill-star">★</span><span
@@ -30,14 +30,14 @@
         </li>
       </ul>
       <h4>Services</h4>
-      <ul>
-        <li><input id="service-spa" type="checkbox"/><label for="service-spa">Spa</label></li>
-        <li><input id="service-swimming-pool" type="checkbox"/><label for="service-swimming-pool">Piscine</label></li>
-        <li><input id="service-restaurant" type="checkbox"/><label for="service-restaurant">Restaurant</label></li>
-        <li><input id="service-hair dryer" type="checkbox"/><label for="service-hair dryer">Sèche-cheveux</label></li>
+      <ul class="margin-0 padding-0 list-unstyled">
+        <li><input id="service-spa" class="cursor-pointer" type="checkbox"/><label for="service-spa">Spa</label></li>
+        <li><input id="service-swimming-pool" class="cursor-pointer" type="checkbox"/><label for="service-swimming-pool">Piscine</label></li>
+        <li><input id="service-restaurant" class="cursor-pointer" type="checkbox"/><label for="service-restaurant">Restaurant</label></li>
+        <li><input id="service-hair dryer" class="cursor-pointer" type="checkbox"/><label for="service-hair dryer">Sèche-cheveux</label></li>
       </ul>
       <h4>Prix</h4>
-      <ul class="price">
+      <ul class="price margin-0 padding-0 list-unstyled">
         <li><input id="price-min" type="number" placeholder="min"/></li>
         <li><input id="price-max" type="number" placeholder="max"/></li>
       </ul>
@@ -56,28 +56,16 @@
 </script>
 <style>
   #filter-desktop {
-    float: left;
     margin: auto;
     padding: 25px;
-    border-right: 1px solid #BDC3C7;
   }
 
   #filter-desktop h4 {
-    padding: 0;
     margin: 0 0 5px;
   }
 
   #filter-desktop ul {
-    padding: 0;
     margin: 0 0 20px;
-  }
-
-  #filter-desktop li {
-    list-style: none;
-  }
-
-  .rating {
-    direction: ltr;
   }
 
   .rating span {
@@ -97,6 +85,5 @@
   input[type="checkbox"] {
     width: 15px;
     height: 15px;
-    cursor: pointer;
   }
 </style>
--- front/src/components/layouts/SearchFiltersMobile.vue
+++ front/src/components/layouts/SearchFiltersMobile.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="hide fixed flex-s flex-x-center flex-y-center flex-column" id="filter-slide">
-    <div id="filter">
+  <div class="hide fixed flex-s flex-x-center flex-y-center flex-column width-100" id="filter-slide">
+    <div id="filter" class="border-bottom border-silver-bottom width-100">
       <form class="flex-form flex-row styled-form">
-        <div class="flex flex-column flex-y-center">
+        <div class="flex flex-column flex-y-center width-50">
           <h4>Classement</h4>
           <ul class="margin-0 padding-0 list-unstyled">
-            <li><input id="ranking-5-star" type="checkbox"/><label for="ranking-5-star">5 étoiles</label></li>
-            <li><input id="ranking-4-star" type="checkbox"/><label for="ranking-4-star">4 étoiles</label></li>
-            <li><input id="ranking-3-star" type="checkbox"/><label for="ranking-3-star">3 étoiles</label></li>
-            <li><input id="ranking-2-star" type="checkbox"/><label for="ranking-2-star">2 étoiles</label></li>
-            <li><input id="ranking-1-star" type="checkbox"/><label for="ranking-1-star">1 étoile</label></li>
+            <li><input id="ranking-5-star" class="cursor-pointer" type="checkbox"/><label for="ranking-5-star">5 étoiles</label></li>
+            <li><input id="ranking-4-star" class="cursor-pointer" type="checkbox"/><label for="ranking-4-star">4 étoiles</label></li>
+            <li><input id="ranking-3-star" class="cursor-pointer" type="checkbox"/><label for="ranking-3-star">3 étoiles</label></li>
+            <li><input id="ranking-2-star" class="cursor-pointer" type="checkbox"/><label for="ranking-2-star">2 étoiles</label></li>
+            <li><input id="ranking-1-star" class="cursor-pointer" type="checkbox"/><label for="ranking-1-star">1 étoile</label></li>
           </ul>
         </div>
-        <div class="flex flex-column flex-y-center">
+        <div class="flex flex-column flex-y-center width-50">
           <h4>Avis client</h4>
           <ul class="margin-0 padding-0 list-unstyled">
             <li>
@@ -34,18 +34,18 @@
             </li>
           </ul>
         </div>
-        <div class="flex flex-column flex-y-center">
+        <div class="flex flex-column flex-y-center width-50">
           <h4>Services</h4>
           <ul class="margin-0 padding-0 list-unstyled">
-            <li><input id="service-spa" type="checkbox"/><label for="service-spa">Spa</label></li>
-            <li><input id="service-swimming-pool" type="checkbox"/><label for="service-swimming-pool">Piscine</label>
+            <li><input id="service-spa" class="cursor-pointer" type="checkbox"/><label for="service-spa">Spa</label></li>
+            <li><input id="service-swimming-pool" class="cursor-pointer" type="checkbox"/><label for="service-swimming-pool">Piscine</label>
             </li>
-            <li><input id="service-restaurant" type="checkbox"/><label for="service-restaurant">Restaurant</label></li>
-            <li><input id="service-hair dryer" type="checkbox"/><label for="service-hair dryer">Sèche-cheveux</label>
+            <li><input id="service-restaurant" class="cursor-pointer" type="checkbox"/><label for="service-restaurant">Restaurant</label></li>
+            <li><input id="service-hair dryer" class="cursor-pointer" type="checkbox"/><label for="service-hair dryer">Sèche-cheveux</label>
             </li>
           </ul>
         </div>
-        <div class="flex flex-column flex-y-center">
+        <div class="flex flex-column flex-y-center width-50">
           <h4>Prix</h4>
           <ul class="margin-0 padding-0 list-unstyled price">
             <li><input id="price-min" type="number" placeholder="min"/></li>
@@ -54,7 +54,7 @@
         </div>
       </form>
     </div>
-    <button class="padding color-clouds">Filtres</button>
+    <button class="padding color-clouds border-bottom border-silver-bottom border-right border-silver-right border-left border-silver-left border-none-top cursor-pointer">Filtres</button>
   </div>
 </template>
 
@@ -95,38 +95,24 @@
 <style>
   #filter-slide {
     z-index: 1;
-    width: 100%;
     transition: top 0.2s;
   }
 
   #filter {
     background-color: #FFFFFF;
-    width: 100%;
     padding: 10px;
-    border-bottom: 1px solid #BDC3C7;
   }
 
   #filter form {
-    display: flex;
-    flex-wrap: wrap;
     max-height: calc(100vh - 150px);
     overflow-y: scroll;
     padding-bottom: 22px;
   }
 
-  #filter form > div {
-    width: 50%;
-  }
-
   #filter-slide button {
     background-color: #FFFFFF;
     color: inherit;
-    cursor: pointer;
     border-radius: 0 0 5px 5px;
-    border-top: none;
-    border-bottom: 1px solid #BDC3C7;
-    border-right: 1px solid #BDC3C7;
-    border-left: 1px solid #BDC3C7;
     width: 100px;
     padding: 15px;
   }
