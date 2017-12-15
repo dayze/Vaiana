50448d7b6eda49ea476ea39c9d27770fa2ce34e8
solenedorey <solene.dorey@outlook.com>
2017-12-15 16:54:07 +0100

    Improving home page design

--- front/src/components/layouts/Services.vue
+++ front/src/components/layouts/Services.vue
@@ -3,8 +3,10 @@
     <div class="container margin-auto">
       <div class="row">
         <div id="first" class="cell-12">
-          <h3 class="color-green-lagoon txt-upper align-center txt-unset txt-size-medium">Lorem ipsum dolor sit amet</h3>
-          <p class="align-center align-justify margin margin-50-top"> Donec porta rutrum risus, vitae hendrerit felis mollis vel.
+          <h3 class="color-green-lagoon txt-upper align-center txt-unset txt-size-medium">Lorem ipsum dolor sit
+            amet</h3>
+          <p class="align-center align-justify margin margin-50-top"> Donec porta rutrum risus, vitae hendrerit felis
+            mollis vel.
             Vestibulum
             lacinia,
             tellus
@@ -15,30 +17,30 @@
             eros.</p>
         </div>
       </div>
-      <hr class="width-30 margin-50-top-bottom border-sunglow border-solid border-thin">
+      <hr class="width-10 margin-50-top-bottom border-solid border-silver border-thin">
       <div class="row padding-30-top flex-y-center flex-x-center">
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-0-l align-center">
-          <icon name="briefcase" scale="4"></icon>
+          <icon name="briefcase" scale="4" class="color-deep-blue"></icon>
         </div>
 
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-1-l flex-item-order-2-s align-center">
-          <icon name="shower" scale="4"></icon>
+          <icon name="shower" scale="4" class="color-green-lagoon"></icon>
         </div>
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-4-l align-center">
-          <icon name="map" scale="4"></icon>
+          <icon name="map" scale="4" class="color-sunglow"></icon>
         </div>
 
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-5-l flex-item-order-6-s align-center">
-          <icon name="bed" scale="4"></icon>
+          <icon name="bed" scale="4" class="color-sweet-potato"></icon>
         </div>
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-2-l flex-item-order-1-s align-center">
           <p class="align-center align-justify margin">
-            Donec porta rutrum risus, vitae hendrerit felis mollis vel. Vestibulum lacinia, tellus
+            <span class="margin-left">Donec</span> porta rutrum risus, vitae hendrerit felis mollis vel. Vestibulum lacinia, tellus
             pulvinar aliquet vehicula, odio neque ornare augue, eu malesuada lorem ligula in neque. Curabitur ac
             interdum
             justo. Morbi ac tristique libero. Cras finibus erat vel diam congue, ut imperdiet dui hendrerit. Cras eu
