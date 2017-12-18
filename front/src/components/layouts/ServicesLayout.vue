e35d7509139e9f058d9bd6e57873a95bd00fa0a9
solenedorey <solene.dorey@outlook.com>
2017-12-18 14:41:05 +0100

    Improving services section on home page

--- front/src/components/layouts/ServicesLayout.vue
+++ front/src/components/layouts/ServicesLayout.vue
@@ -26,7 +26,26 @@
 
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-1-l flex-item-order-2-s align-center">
-          <icon name="shower" scale="4" class="color-green-lagoon"></icon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 494 494">
+            <path class="color-green-lagoon"
+              d="M246.7 95.3c54.2-16 95.1-39.3 87.7-64.3C327.1 6 231.6-9.5 177.3 6.5c-54.2 16-46.6 57.4-39.2 82.4C145.5 113.9 192.5 111.3 246.7 95.3z"/>
+            <path class="color-green-lagoon"
+              d="M139.5 170c-7.8 31.3 78 42 155.1 61.2 77.1 19.2 142.8 21.8 150.6-9.5 7.8-31.3 14.3-83-62.7-102.2C305.3 100.3 147.3 138.7 139.5 170z"/>
+            <path class="color-green-lagoon"
+              d="M186.6 240.8c-15.9 0-29 2.2-40.1 5.9 16.1 9.9 25.3 30.3 26.7 49 22-15.7 52.6-12.2 67 12.7 14.5 25.2 0.9 53.8-23.4 70.6 12 5.3 21.2 15.3 25.6 26.6 3.1 0 6.2 0.1 9.4 0.1 89 0 226.3-17 226.3-58C478.1 306.6 275.6 240.8 186.6 240.8z"/>
+            <path class="color-green-lagoon"
+              d="M123.6 363.6c27.8-15.7 37.8-52.3 32.1-75 -4.3-17.2-14.4-32.2-32.1-32.2 -17.8 0-28.6 14.2-28.6 31.9 0 6.2 3.4 12.8 7.7 20.3C112.3 325 122.7 342.2 123.6 363.6z"/>
+            <path class="color-green-lagoon"
+              d="M122.2 386.9c-17.4 9.8-27.9 27.9-31.8 45.7 -2.3 10.4-2.4 20.8-0.3 29.3 4.3 17.2 14.4 32.2 32.2 32.2 17.7 0 28.6-14.2 28.6-31.9 0-6.2-3.4-12.8-7.7-20.2C133.6 425.5 123.1 408.4 122.2 386.9z"/>
+            <path class="color-green-lagoon"
+              d="M45 359c16.3 1.6 42.9-3.1 68.2 9.9 0-1.7-0.1-6.3-0.6-6.5 -3.1-28.9-27.1-52.7-48.2-58.8 -17-4.9-35.2-3.3-43.9 11.8C9 335 24.3 356.9 45 359z"/>
+            <path class="color-green-lagoon"
+              d="M190 391.1c0 0 0 0 0 0 -16 0.1-36.3 1.6-57.4-9.4 -0.3 35.7 37.3 76 73.3 67.1 5.9-1.5 15.4-6.6 19.5-13.6C235.8 417.2 225.6 391.1 190 391.1z"/>
+            <path class="color-green-lagoon"
+              d="M112.5 380.4c-12.9-7.6-29.7-10.6-49-6.3 -11.4 2.5-23.8 8-32 15.9 -22.1 21.2-18.7 43.6-2.3 53.8 10.3 6.4 22.4 4.7 32.4-1.1C77 433.9 81.2 400.4 112.5 380.4z"/>
+            <path class="color-green-lagoon"
+              d="M178.9 377.2c12.1-2.1 26.7-8.3 35.5-16.8 15.5-14.9 22-34.4 7.5-49.6 -8.7-9.2-23.7-11.2-37.7-3.1 -15.4 8.9-19.6 42.4-50.8 62.4C145.2 377.1 161.4 380.3 178.9 377.2z"/>
+          </svg>
         </div>
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-4-l align-center">
@@ -39,8 +58,10 @@
         </div>
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-2-l flex-item-order-1-s align-center">
+          <h3 class="txt-upper txt-unset txt-size-medium">Business</h3>
           <p class="align-center align-justify margin">
-            <span class="margin-left">Donec</span> porta rutrum risus, vitae hendrerit felis mollis vel. Vestibulum lacinia, tellus
+            <span class="margin-left">Donec</span> porta rutrum risus, vitae hendrerit felis mollis vel. Vestibulum
+            lacinia, tellus
             pulvinar aliquet vehicula, odio neque ornare augue, eu malesuada lorem ligula in neque. Curabitur ac
             interdum
             justo. Morbi ac tristique libero. Cras finibus erat vel diam congue, ut imperdiet dui hendrerit. Cras eu
@@ -51,6 +72,7 @@
 
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-3-l flex-item-order-3-s align-center">
+          <h3 class="txt-upper txt-unset txt-size-medium">Bien-être</h3>
           <p class="align-center align-justify margin">
             Donec porta rutrum risus, vitae hendrerit felis mollis vel. Vestibulum lacinia, tellus
             pulvinar aliquet vehicula, odio neque ornare augue, eu malesuada lorem ligula in neque. Curabitur ac
@@ -63,6 +85,7 @@
 
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-6-l flex-item-order-5-s align-center">
+          <h3 class="txt-upper txt-unset txt-size-medium">Evasion</h3>
           <p class="align-center align-justify margin">
             Donec porta rutrum risus, vitae hendrerit felis mollis vel. Vestibulum lacinia, tellus
             pulvinar aliquet vehicula, odio neque ornare augue, eu malesuada lorem ligula in neque. Curabitur ac
@@ -75,6 +98,7 @@
 
 
         <div class="cell-3 c-6-l c-12-s flex-item-order-7-l align-center">
+          <h3 class="txt-upper txt-unset txt-size-medium">Repos</h3>
           <p class="align-center align-justify margin">
             Donec porta rutrum risus, vitae hendrerit felis mollis vel. Vestibulum lacinia, tellus
             pulvinar aliquet vehicula, odio neque ornare augue, eu malesuada lorem ligula in neque. Curabitur ac
